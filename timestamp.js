394c93d283c0953a4c26d9ac144ee578c5961f74
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-03-25 16:13:09 +0300

    date and time

--- timestamp.js
+++ timestamp.js
@@ -1,3 +1,10 @@
 let d = new Date();
 let timestamp = d.getTime();
 console.log(timestamp);
+
+// Today - Date
+console.log(d.toLocaleDateString('en-GB'));
+// today - time
+console.log(d.toLocaleTimeString('en-GB'));
+// tomorrow - date
+console.log(d.toLocaleDateString('en-GB',d.setDate(d.getDate()+1)));
